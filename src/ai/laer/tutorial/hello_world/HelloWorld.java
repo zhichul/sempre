d27d2538b7b23703f8042aa4273463c5be1e8a58
Brian Lu <zhichulu75@gmail.com>
2018-11-24 21:29:45 -0500

    add hello_world method

---
+++ src/ai/laer/tutorial/hello_world/HelloWorld.java
@@ -0,0 +1,8 @@
+package ai.laer.tutorial.hello_world;
+
+public class HelloWorld {
+    public static boolean sayHello() {
+        System.out.println("Hello World!");
+        return true;
+    }
+}
